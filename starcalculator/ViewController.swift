91def80569d928a7b1f602c3b951f34d2cbbfd11
Emre Dogan <emredogan@outlook.com>
2016-04-22 23:32:32 +0200

    Initial Commit

---
+++ starcalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  starcalculator
+//
+//  Created by Emre Dogan on 22/04/16.
+//  Copyright © 2016 Emre Dogan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
